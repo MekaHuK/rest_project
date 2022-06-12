429b10af9c33b52e1d56205a4d615db7e453ad55
Intergalactic <intergalactic2006@ya.ru>
2022-06-12 15:17:46 +0300

    —The controller has been rewritten —All server responses are transmitted in the JSON format —A new data structure has been created to provide a response —The logic from the controller has been moved to the service layer —Removed artificial positive server responses —Methods renamed —The search mechanism for two parameters has been changed - filtering takes place in the database —Access modifiers have been added to DTO fields —Redundant code removed —Added ReqestMapping annotation

--- src/main/java/com/rest_project/repository/TransactionRepository.java
+++ src/main/java/com/rest_project/repository/TransactionRepository.java
@@ -2,7 +2,19 @@ package com.rest_project.repository;
 
 import com.rest_project.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.Collection;
+import java.util.List;
 
 public interface TransactionRepository extends JpaRepository<Transaction, Integer>{
 
+    @Query("SELECT t FROM Transaction t WHERE t.status = ?1 AND t.content = ?2")
+    List<Transaction> findByContentAndStatus(String status, String content);
+
+    @Query("SELECT t FROM Transaction t WHERE t.status = ?1")
+    List<Transaction> findAllByStatus(String status);
+
+    @Query("SELECT t FROM Transaction t WHERE t.content = ?1")
+    List<Transaction> findAllByContent(String content);
 }
--- src/main/java/com/rest_project/service/TransactionServiceImpl.java
+++ src/main/java/com/rest_project/service/TransactionServiceImpl.java
@@ -1,23 +1,18 @@
 package com.rest_project.service;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import com.rest_project.controller.controlleradvice.Violation;
+import com.rest_project.controller.TransactionsSuccessResponse;
 import com.rest_project.dto.TransactionDto;
 import com.rest_project.model.Transaction;
 import com.rest_project.utils.MappingUtils;
 import com.rest_project.repository.TransactionRepository;
 import net.minidev.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.MethodParameter;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-
+import java.util.HashMap;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class TransactionServiceImpl implements TransactionService{
@@ -26,7 +21,7 @@ public class TransactionServiceImpl implements TransactionService{
     private TransactionRepository transactionRepository;
 
     @Override
-    public JSONObject create(TransactionDto transactionDto){
+    public JSONObject createTransaction(TransactionDto transactionDto){
         Transaction transaction = MappingUtils.mapToTransaction(transactionDto);
         transactionRepository.save(transaction);
         JSONObject transactionJson = new JSONObject();
@@ -35,85 +30,47 @@ public class TransactionServiceImpl implements TransactionService{
     }
 
     @Override
-    public List<String> readAll() throws JsonProcessingException {
-        List<TransactionDto> result = transactionRepository.findAll().stream()
-                .map(MappingUtils::mapToTransactionDTO)
-                .collect(Collectors.toList());
-
-        List<String> transactionsJson = new ArrayList<>();
-        for (TransactionDto dto : result){
-            ObjectMapper objectMapper = new ObjectMapper();
-            objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-            objectMapper.registerModule(new JavaTimeModule());
-            transactionsJson.add(objectMapper.writeValueAsString(dto));
-        }
-        return transactionsJson;
-    }
-
-    @Override
-    public String read(int id) throws JsonProcessingException {
-
-        TransactionDto result = MappingUtils.mapToTransactionDTO(transactionRepository.getOne(id));
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-        objectMapper.registerModule(new JavaTimeModule());
-        return objectMapper.writeValueAsString(result);
-    }
-
-    @Override
-    public List<String> statusFilter(String status) throws JsonProcessingException {
-        List<Transaction> dtoList = new ArrayList<>();
-        List<Transaction> allTransactions = transactionRepository.findAll();
-        for(Transaction transaction : allTransactions){
-            if(transaction.getStatus().equals(status)){
-                dtoList.add(transaction);
+    public TransactionsSuccessResponse readAllTransactions() throws JsonProcessingException {
+        TransactionsSuccessResponse response = new TransactionsSuccessResponse();
+            for(Transaction transaction : transactionRepository.findAll()){
+                response.getTransactions().add(MappingUtils.mapToTransactionDTO(transaction));
             }
-        }
-        List<TransactionDto> result = dtoList.stream()
-                .map(MappingUtils::mapToTransactionDTO)
-                .collect(Collectors.toList());
-
-        List<String> transactionsJson = new ArrayList<>();
-        for (TransactionDto dto : result){
-            ObjectMapper objectMapper = new ObjectMapper();
-            objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-            objectMapper.registerModule(new JavaTimeModule());
-            transactionsJson.add(objectMapper.writeValueAsString(dto));
-        }
-        return transactionsJson;
+        return response;
     }
 
     @Override
-    public List<String> complexFilter(String string) throws JsonProcessingException {
-        List<Transaction> dtoList = new ArrayList<>();
-        List<Transaction> allTransactions = transactionRepository.findAll();
-        for(Transaction transaction : allTransactions){
-            if(transaction.getStatus().equals(string) || transaction.getContent().equals(string)){
-                dtoList.add(transaction);
-            }
-        }
-        List<TransactionDto> result = dtoList.stream()
-                .map(MappingUtils::mapToTransactionDTO)
-                .collect(Collectors.toList());
+    public TransactionsSuccessResponse searchByParams(HashMap<String, String> requestParams){
+        TransactionsSuccessResponse response = new TransactionsSuccessResponse();
+        List<Transaction> transactions = new ArrayList<>();
 
-        List<String> transactionsJson = new ArrayList<>();
-        for (TransactionDto dto : result){
-            ObjectMapper objectMapper = new ObjectMapper();
-            objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
-            objectMapper.registerModule(new JavaTimeModule());
-            transactionsJson.add(objectMapper.writeValueAsString(dto));
+        if(requestParams.containsKey("content") && requestParams.containsKey("status")){
+            transactions.addAll(transactionRepository.findByContentAndStatus(requestParams.get("status"), requestParams.get("content")));
+
+        } else if(requestParams.containsKey("status")){
+            transactions.addAll(transactionRepository.findAllByStatus(requestParams.get("status")));
+
+        } else if(requestParams.containsKey("content")){
+            transactions.addAll(transactionRepository.findAllByContent(requestParams.get("content")));
         }
-        return transactionsJson;
+
+        for(Transaction transaction : transactions){
+            response.getTransactions().add(MappingUtils.mapToTransactionDTO(transaction));
+        }
+        return response;
+    }
+
+    @Override
+    public TransactionDto readOneTransaction(int id) throws JsonProcessingException {
+        if(!transactionRepository.existsById(id)){
+            throw new ResourceNotFoundException();
+        }
+        return MappingUtils.mapToTransactionDTO(transactionRepository.getOne(id));
+
     }
 
     @Override
     public boolean existsById(int id){
         return transactionRepository.existsById(id);
-    }
-
-    @Override
-    public boolean existsAny(){
-        return transactionRepository.count() > 0;
     }
 
 }
